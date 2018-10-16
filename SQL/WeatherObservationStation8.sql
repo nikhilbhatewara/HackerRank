0a88959ffcaaf3d6d781ee9c2a932e29ba7f5dd4
Nikhil Bhatewara <jain2nikhil@gmail.com>
2018-10-16 02:25:19 -0500

    Update WeatherObservationStation8.sql

--- SQL/WeatherObservationStation8.sql
+++ SQL/WeatherObservationStation8.sql
@@ -1,1 +1,16 @@
 
+/*
+Enter your query here.
+*/
+
+/*
+Enter your query here.
+*/
+
+/*
+Enter your query here.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE SUBSTRING(CITY, -1 ,1) IN ('a','e','i','o','u','A','E','I','O','U')  and SUBSTRING(CITY, 1 ,1) IN ('a','e','i','o','u','A','E','I','O','U') 
