11c40a28d7f300e21d330c8c79cd7f4a5e498a86
Nikhil Bhatewara <jain2nikhil@gmail.com>
2018-10-17 14:08:49 -0500

    Create BinaryTreeNodes.sql

---
+++ SQL/BinaryTreeNodes.sql
@@ -0,0 +1,16 @@
+/*
+Enter your query here.
+
+SELECT *
+FROM BST;
+*/
+
+
+SELECT N, 
+CASE
+    WHEN P IS NULL THEN "Root"
+    WHEN N IN (SELECT P FROM BST) THEN "Inner"
+    ELSE "Leaf"
+END
+FROM BST
+ORDER BY N;
